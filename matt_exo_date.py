9b47451c11144cad292c7567e2205dd8cd03b181
mlamamra <mlamamra@dawan.fr>
2025-09-08 16:48:47 +0200

    j1: numeric: exo date : IF / ELSE

--- matt_exo_date.py
+++ matt_exo_date.py
@@ -10,11 +10,22 @@ nb_horaire = input("saisir un entier au clavier => compris entre 0 et 86400")
 # REM: python est un langage avec typage dynamique
 # => le type d'une variable peut changer 
 nb_horaire = int(nb_horaire)
-nb_heure = nb_horaire // 3600
-nb_minute = (nb_horaire % 3600) // 60
-nb_seconds =nb_horaire % 60
 
-print(str(nb_heure) + "h " + str(nb_minute) + "m " + str(nb_seconds) + "s")
+## traitement conditionnel
+
+# if <expression>:
+if 0 <= nb_horaire < 86400:
+  # dans le bloc if => le if est vrai
+  nb_heure = nb_horaire // 3600
+  nb_minute = (nb_horaire % 3600) // 60
+  nb_seconds =nb_horaire % 60
+
+  print(str(nb_heure) + "h " + str(nb_minute) + "m " + str(nb_seconds) + "s")
+# elif <expr>: (else if)
+else:
+  print( str(nb_horaire) + ": mauvaise valeur !")
+# sortie du bloc if => continuation du programme
+print("FIN")
 # %%
 # expression logique
 nb_horaire >= 0 and nb_horaire < 86400
@@ -24,3 +35,14 @@ nb_horaire >= 0 and nb_horaire < 86400
 not (nb_horaire < 0 or nb_horaire >= 86400)
 
 # %%
+
+nb_horaire = input("saisir un entier au clavier => compris entre 0 et 86400")
+nb_horaire = int(nb_horaire)
+
+if 0 <= nb_horaire < 86400:
+  nb_heure = nb_horaire // 3600
+  nb_minute = (nb_horaire % 3600) // 60
+  nb_seconds =nb_horaire % 60
+  print(str(nb_heure) + "h " + str(nb_minute) + "m " + str(nb_seconds) + "s")
+else:
+  print( str(nb_horaire) + ": mauvaise valeur !")
