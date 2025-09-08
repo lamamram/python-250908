1689d053b35713ba7e96fbea8abea800286deab5
mlamamra <mlamamra@dawan.fr>
2025-09-08 17:34:16 +0200

    j1: sequences: operateur []

---
+++ matt_cours_sequences.py
@@ -0,0 +1,13 @@
+# %%
+## SEQUENCE => str, list, tuple
+
+dicton = "appeler un chat un chat"
+mots = ["appeler", "un", "chat", "un", "chat"]
+tups = tuple(mots)
+
+print(dicton, mots, tups)
+# %%
+## opérateur []: lire les éléments d'une séquence à l'index i
+# 0-indexed => 1er index de la séquence
+dicton[0], mots[0], tups[0]
+# %%
