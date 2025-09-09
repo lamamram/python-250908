a46b6107cfcc29edcc61ddaf81af55ff69903ba6
mlamamra <mlamamra@dawan.fr>
2025-09-09 14:36:29 +0200

    j2: boucles: break, continue, else

--- matt_cours_boucles.py
+++ matt_cours_boucles.py
@@ -76,3 +76,29 @@ print("BOOM")
 
 
 # %%
+# les mots continue, break, else et pass pour while et for
+# break sort directement de la boucle 
+# pass : instruction qui ne fait rien (bouche trou / factotum)
+
+temp = int(input("temp °C"))
+
+while temp < 100:
+  # pass
+  if temp == 42:
+    print("valeur maudite !")
+    break
+  if temp <= 33:
+    print(f"{temp}: trop froid")
+    temp += 10
+    # interrompt le bloc courant et commence l'itération suivante
+    continue
+  print(f"{temp}: çà fonctionne")
+  temp = int(input("temp °C"))
+# exécute le bloc ci-dessous SI la boucle s'arrête normalement
+# Si la boucle ne rencontre pas de break
+else:
+  print("trop chaud ! BOOM")
+
+
+
+# %%
