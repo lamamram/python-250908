cfdbd90f37474bf0bdb6ca7ceb80f621f1ef86bc
mlamamra <mlamamra@dawan.fr>
2025-09-10 16:16:32 +0200

    j3: poo: exemple cannonique en poo

--- matt_exo_poo.py
+++ matt_exo_poo.py
@@ -20,7 +20,7 @@
 
 
 if __name__ == "__main__":
-    text = """
+   text = """
 Python est un langage de programmation interprété, 
 multiparadigme et multiplateformes. 
 Il favorise la programmation impérative structurée, 
@@ -38,6 +38,6 @@ par macOS, ou encore Android, iOS, et peut aussi être traduit en Java ou .NET.
 Il est conçu pour optimiser la productivité des programmeurs en offrant des outils de haut niveau et 
 une syntaxe simple à utiliser. 
 """
-
+  
 
 # %%
