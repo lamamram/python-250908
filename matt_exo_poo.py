af9a9471d7bccb6fdb3cc155dfabb3e1f1d8e324
mlamamra <mlamamra@dawan.fr>
2025-09-10 16:23:35 +0200

    j3: exo poo: squelette

--- matt_exo_poo.py
+++ matt_exo_poo.py
@@ -17,10 +17,10 @@
 5. écrire le programme principal pour utiliser le package
 """
 # %%
-
+from matt_text_analyzer.text_cleaner import Cleaner
 
 if __name__ == "__main__":
-   text = """
+  text = """
 Python est un langage de programmation interprété, 
 multiparadigme et multiplateformes. 
 Il favorise la programmation impérative structurée, 
@@ -38,6 +38,8 @@ par macOS, ou encore Android, iOS, et peut aussi être traduit en Java ou .NET.
 Il est conçu pour optimiser la productivité des programmeurs en offrant des outils de haut niveau et 
 une syntaxe simple à utiliser. 
 """
+  
+  cleaner = Cleaner()
   
 
 # %%
