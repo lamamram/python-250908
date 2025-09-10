1ded6b1a4448c29b4889a3cf8a9b8874e92f55e2
mlamamra <mlamamra@dawan.fr>
2025-09-10 16:51:47 +0200

    j3: exo poo: cleaner 3/3

--- matt_text_analyzer/text_cleaner.py
+++ matt_text_analyzer/text_cleaner.py
@@ -8,6 +8,23 @@ class Cleaner:
   def __clean_punctuation(self):
     self.__text = re.sub(f"[{string.punctuation}]", " ", self.__text)
 
+  def __clean_breaks(self):
+    # raw string: qui traite le \ comme \
+    self.__text = re.sub(r"\r?\n", " ", self.__text)
+  
+  def __clean_spaces(self):
+    self.__text = re.sub(" +", " ", self.__text)
+  
+  def __remove_little_words(self):
+    words = []
+    for word in self.__text.split():
+      if len(word) > 3:
+        words.append(word)
+    self.__text = " ".join(words)
+
   def clean(self) -> str:
     self.__clean_punctuation()
+    self.__clean_breaks()
+    self.__clean_spaces()
+    self.__remove_little_words()
     return self.__text
