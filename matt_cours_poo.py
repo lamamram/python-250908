699dd503f84a1fe0de11b0bd426095ba0c8fd1c0
mlamamra <mlamamra@dawan.fr>
2025-09-10 15:25:15 +0200

    j3: poo: 1er exemple en poo

--- matt_cours_poo.py
+++ matt_cours_poo.py
@@ -16,4 +16,32 @@ if __name__ == "__main__":
   }
   account = withdrawal(account, -50)
   print(account)
+
+# %%
+##### première écriture en POO
+class Account:
+  # attributs
+  _id = 0
+  balance = 0.
+  overdraft = 0.
+
+  # méthodes
+  ## pour chaque méthode: le premier paramètre obligatoire est self
+  ## qui représente l'objet instancié de la classe
+  def withdrawal(self, amount: float) -> dict:
+    if amount > 0:
+      self.balance -= amount
+    else:
+      print(f"montant invalide: {amount}")
+
+if __name__ == "__main__":
+  # instanciation: création d'un objet de la classe Account
+  acc = Account()
+  acc._id = 14343
+  acc.balance = 100.
+  acc.overdraft = 100.
+
+  acc.withdrawal(50)
+  acc.balance
+
 # %%
