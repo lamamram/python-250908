857e1e946c8d0ed17eaa1154aa8e69fca14071cf
mlamamra <mlamamra@dawan.fr>
2025-09-11 17:03:55 +0200

    j4: executer un script SQL avec gestion d'erreur

--- matt_atelier_db.py
+++ matt_atelier_db.py
@@ -27,3 +27,25 @@ with sqlite3.connect("../dns.db") as conn:
 # close
 # %%
 ######## insertion des tables à partir d'un script
+import os, sqlite3
+
+def execute_script(script_path, conn: sqlite3.Connection):
+  if not os.path.exists(script_path):
+    raise FileNotFoundError("script absent")
+  with open(script_path, "r", encoding="utf-8") as sql_f:
+    script = sql_f.read()
+  ## contexte réentrant (multiple with) pour être sûr d'être ouvert
+  with conn:
+    cur = conn.cursor()
+    try:
+      cur.executescript(script)
+      return True, "OK"
+    except sqlite3.OperationalError as oe:
+      return False, oe
+
+if __name__ == "__main__":
+
+  with sqlite3.connect("../dns.db") as conn:
+    ret = execute_script("./domain_names_sqlite3.sql", conn)
+    print(ret)
+# %%
