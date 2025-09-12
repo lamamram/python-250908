d47e0374427e88d5164833480a73953c9c3a4c5f
mlamamra <mlamamra@dawan.fr>
2025-09-12 11:52:00 +0200

    j5: requests: consigne exercice REST

--- matt_atelier_http.py
+++ matt_atelier_http.py
@@ -28,3 +28,35 @@ else:
         ## JSON
         # response.json()
 # %%
+###### EXERCICE API REST
+"""
+client vers https://gorest.co.in
+1. écrire une classe GoRestClient attributs attendus 
+   - le nom de domaine
+   - la version
+2. concentrer l'utilisation de requests dans une méthode privée __call
+qui doit gérer tous les types de requêtes (GET, POST, PUT)
+Hint: utiliser la fonction getattr
+3. ajouter une méthode publique get_users qui va accumuler les résultats
+   des appels à l'endpoint https://gorest.co.in/public/v2/users à pagigner
+   jusqu'on ait tous les utilisateurs
+"""
+# %%
+###### les fonctions getattr et setattr hasattr
+
+class Truc:
+  pass
+
+tr = Truc()
+# création dynamique d'un attribut à partir d'une str
+setattr(tr, "param", 10)
+
+tr.param
+# retourner la valeur d'un attribut via une str
+getattr(tr, "param")
+hasattr(tr, "param")
+
+nom = "joe"
+# getattr(nom, "upper") est un attribut de type fonction/méthode
+getattr(nom, "upper")()
+# %%
